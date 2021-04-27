a657cdaa0859924132ebcca395f7a3b089009fae
Juand0145 <juand0145@gmail.com>
2021-04-27 12:55:15 -0500

    solving task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,15 @@ if number < 0:
         if digit == 0:
                 print("Digit digit of {} is {} and is 0".format(number, digit))
         elif digit < 6 and digit != 0:
-                print("Digit digit of {} is {} and is less than 6 and not 0".format(number, digit))
+                print("Digit digit of {} is {} and is less than 6 and not 0"
+                      .format(number, digit))
 else:
         digit = number % 10
         if digit > 5:
-                print("Digit digit of {} is {} and is greater than 5".format(number, digit))
+                print("Digit digit of {} is {} and is greater than 5"
+                      .format(number, digit))
         elif digit == 0:
                 print("Digit digit of {} is {} and is 0".format(number, digit))
         elif digit < 6 and digit != 0:
-                print("Digit digit of {} is {} and is less than 6 and not 0".format(number, digit))
+                print("Digit digit of {} is {} and is less than 6 and not 0"
+                      .format(number, digit))
