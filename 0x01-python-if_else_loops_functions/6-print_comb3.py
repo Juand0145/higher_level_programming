1c0d1ac3c3c6b3374d4207470262142f56095faf
Juand0145 <juand0145@gmail.com>
2021-04-27 16:25:14 -0500

    Adding task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i == 8 and j == 9):
+            print("89")
+        if (i != j and i < j):
+            print("{}{}".format(i, j), end=", ")
