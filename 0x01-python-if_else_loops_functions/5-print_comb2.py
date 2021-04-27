13febd47970480d5b080ffbf832ffc459e1571bd
Juand0145 <juand0145@gmail.com>
2021-04-27 16:09:56 -0500

    pep 8 issue

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number == 99:
         print(number)
     else:
-        print("{:0>2d}".format(number), end=", ")
+        print("{:02}".format(number), end=", ")
