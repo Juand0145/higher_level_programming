9288ca28eb0880f5696a2a5d7569fba1082d5eec
Juand0145 <juand0145@gmail.com>
2021-05-05 16:58:48 -0500

    SOlving task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     n_elemets = len(my_list)
 
     if (idx < 0 or idx > n_elemets - 1):
-        return None
+        return my_list
 
     my_list[idx] = element
 
