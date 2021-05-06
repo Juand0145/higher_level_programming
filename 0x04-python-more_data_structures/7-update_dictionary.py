c6739ebf5e17f9f71b39ab5da44aab5d0b25d9db
Juand0145 <juand0145@gmail.com>
2021-05-06 16:53:47 -0500

    Adding task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None:
+        return None
+
+    a_dictionary.update({key: value})
+
+    return a_dictionary
