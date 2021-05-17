856d55d8b31dc2ea072228783bba2c3d5123bcc8
Juand0145 <juand0145@gmail.com>
2021-05-17 12:08:23 -0500

    IndexExrror problem

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except (TypeError, ValueError):
-            continue
+        except (TypeError, ValueError, IndexError):
+            pass
 
     print()
     return counter
