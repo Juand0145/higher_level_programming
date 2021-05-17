ccc088d186a2106345a6e50a809313abea2d2709
Juand0145 <juand0145@gmail.com>
2021-05-17 11:18:28 -0500

    Adding task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhon3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+
+    while (i < x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            i += 1
+
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return i
