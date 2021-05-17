fe9a203c460685b9d9d2420aeb194071f7b52588
Juand0145 <juand0145@gmail.com>
2021-05-17 12:05:40 -0500

    fixing pep 8 problem

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/pyhon3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-
-    while (i < x):
+    counter = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-            i += 1
-
-        except (ValueError, TypeError):
-            pass
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except (TypeError, ValueError):
+            continue
 
     print()
-    return i
+    return counter
