e50dc1fd94050ab48b2e3cd4d616c3c36018fefe
Juand0145 <juand0145@gmail.com>
2021-05-18 18:18:45 -0500

    Adding task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Create a square class"""
+
+
+class Square:
+    """Creating a private instance atribute: size"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+    def area(self):
+        """A new Method call: area= size ^ 2"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Making public size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """giving the property setter"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Print a square of # give it the respective size"""
+        if self.__size == 0:
+            print()
+
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+
+                print()
