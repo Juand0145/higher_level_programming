4fab44c2fa472a47f6f136ee6d326c72a640bf47
Juand0145 <juand0145@gmail.com>
2021-05-20 20:27:07 -0500

    fixing pep8

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def is_safe(board, row, column):
 
-    for queen in range(column):
-        if board[queen] == row or abs(board[queen] - row) == abs(queen - column):
+    for Que in range(column):
+        if board[Que] == row or abs(board[Que] - row) == abs(Que - column):
             return False
 
     return True
@@ -30,6 +30,10 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
         sys.exit(1)
 
     board = [0 for row in range(n)]
