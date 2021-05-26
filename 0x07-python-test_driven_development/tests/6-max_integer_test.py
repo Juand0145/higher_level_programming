bf715b6520930f024b8bbc176828ecd568af8e65
Juand0145 <juand0145@gmail.com>
2021-05-26 09:51:02 -0500

    Correcting task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,53 +8,53 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def normal_result(self):
+    def test_normal(self):
         array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         result = max_integer(array)
         self.assertAlmostEqual(result, 10)
 
-    def not_int(self):
+    def test_notint(self):
         """Not Intes"""
         array = ["a", "b", 9]
         self.assertRaises(TypeError, max_integer, array)
 
-    def number_empty(self):
+    def test_numberempty(self):
         """Empty list: should return None"""
         array = []
         result = max_integer(array)
         self.assertEqual(result, None)
 
-    def number_negative(self):
+    def test_numbernegative(self):
         """Negative values"""
         array = [-8, -9, -3]
         result = max_integer(array)
         self.assertEqual(result, -3)
 
-    def number_float(self):
+    def test_numberfloat(self):
         """Ints and floats"""
         array = [1, 2.7, 3]
         result = max_integer(array)
         self.assertEqual(result, 3)
 
-    def one_number(self):
+    def test_onenumber(self):
         """One number"""
         array = [7]
         result = max_integer(array)
         self.assertEqual(result, 7)
 
-    def identical(self):
+    def test_identical(self):
         """Identical values"""
         array = [1, 1, 1, 1, 1]
         result = max_integer(array)
         self.assertEqual(result, 1)
 
-    def strings(self):
+    def test_strings(self):
         """list of strings: return the first string"""
         array = ["I", "like"]
         result = max_integer(array)
-        self.assertEqual(result, "I")
+        self.assertEqual(result, "like")
 
-    def none(self):
+    def test_none(self):
         """None as parameter:"""
         self.assertRaises(TypeError, max_integer, None)
 
