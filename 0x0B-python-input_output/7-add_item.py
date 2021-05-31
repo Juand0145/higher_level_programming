0bd5d946a24e2beb17249427b4b5d7dab126da80
Juand0145 <juand0145@gmail.com>
2021-05-31 14:15:35 -0500

    Correcting pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' '''
+'''A script that adds all arguments to a Python list,
+and then save them to a file '''
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -16,9 +17,8 @@ with open(file_name, mode="a") as my_file:
     elif my_file.tell() == 0 and len(sys.argv) > 1:
         data.extend(sys.argv[1:])
         save_to_json_file(data, file_name)
-    
+
     elif len(sys.argv) > 1:
         data = load_from_json_file(file_name)
         data.extend(sys.argv[1:])
         save_to_json_file(data, file_name)
-
