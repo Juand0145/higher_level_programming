303ca84482009fdd97be346c74685088fa7a2a47
Juand0145 <juand0145@gmail.com>
2021-06-06 17:57:32 -0500

    Creating directory and adding some task

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,15 @@
+## Python - Almost a circle
+In this directory, we will find a compilation of files that will help us to understand and how to use some aplicatiion in the OOP in Python and help us to answer the next questions:
+
+  -   What is Unit testing and how to implement it in a large project
+  -   How to serialize and deserialize a Class
+  -   How to write and read a JSON file
+  -   What is  `*args`  and how to use it
+  -   What is  `**kwargs`  and how to use it
+  -   How to handle named arguments in a function
+## Files
+ - tests/ All files, classes and methods were tested and PEP 8 validated.
+ - models/base.py, models/__init__.py This class will be the “base” of all other classes in this project. The goal of it is to manage `id` attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
+ - models/rectangle.py 
+ - models/square.py
+ - models/base.py
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+'''Module Rectangle'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''Class that represent a rectangle'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Initializing Rectangle:
+        Arguments:
+            width, hegight = dimensions of he rectangle
+            x, y = The position of the rectangle'''
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        '''width atribute'''
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        '''Asigning atribute width'''
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+
+        if width <= 0:
+            raise ValueError ("width must be > 0")
+
+    @property
+    def height(self):
+        '''height atribute'''
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        '''Asigning atribute height'''
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+
+        if height <= 0:
+            raise ValueError ("height must be > 0")
+
+    @property
+    def x(self):
+        '''x atribute'''
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        '''Asigning atribute x'''
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+
+        if x < 0:
+            raise ValueError ("x must be >= 0")
+
+    @property
+    def y(self):
+        '''y atribute'''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''Asigning atribute x'''
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError ("y must be >= 0")
+
+    def area(self):
+        '''Public instance Method that evaluate the area of the rectangle'''
+        area = self.__height * self.__width
+        return area
+
+    def display(self):
+        '''Public instance Method that print and rectangle with #'''
+        if self.__height == 0 or self.__width == 0:
+            print('')
+
+        else:
+            if self.__y > 0:
+                print ((self.__y - 1) * '\n')
+            for i in range(self.__height):
+                print(self.__x * ' ' + self.__width * '#')
+
+    def __str__(self):
+        '''Create a string that defines the rectangle'''
+        data = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id,
+            self.__x,
+            self.__y,
+            self.__width,
+            self.__height)
+
+        return data
+
+    
+
