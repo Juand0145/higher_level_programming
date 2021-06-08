acafb962b83cd899b9d61d39a202810aef1ce41d
Juand0145 <juand0145@gmail.com>
2021-06-08 17:46:58 -0500

    Fixing pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""Unittest for the super class called 'Base'
-"""
+'''tst file were are analizing the function of the rectagle'''
 import unittest
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    """Class that declares the test functions to test if the
-    'Rectangle' class is working correctly.
-    """
-    def test_rectangle_class(self):
-        """Function with some test cases"""
+    '''Tsting the parent class Rectangle'''
+
+    def test_rectangle(self):
+        '''Testing the creation of the rectangle'''
         r1 = Rectangle(10, 3)
         self.assertEqual(3, r1.height)
         self.assertEqual(10, r1.width)
-
         r2 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(12, r2.id)
         self.assertEqual(0, r1.x)
 
-    def test_area(self):
-        """Testing the area() function"""
+    def test_area_rectangle(self):
+        '''Testing the publci method area were we are finding the
+        area of a rectangle'''
         r1 = Rectangle(3, 2)
         self.assertEqual(6, r1.area())
         r2 = Rectangle(8, 7, 0, 0, 12)
         self.assertEqual(56, r2.area())
 
-    def test_update(self):
-        """Testing the update() function"""
+    def test_update_rectangle(self):
+        '''Testing the update method'''
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(89)
         self.assertEqual(89, r1.id)
@@ -40,8 +38,6 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(3, r1.height)
         self.assertEqual(4, r1.x)
         self.assertEqual(5, r1.y)
-
-        # update with kwargs
         r2 = Rectangle(5, 5, 5, 5)
         r2.update(height=1)
         self.assertEqual(1, r2.height)
@@ -51,10 +47,9 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(3, r2.y)
         self.assertEqual(4, r2.width)
 
-    def test_to_dictionary(self):
-        """Testing to-dictionary() function"""
+    def test_dictionary(self):
+        '''Testing to-dictionary() function'''
         r1 = Rectangle(10, 2, 1, 9, 12)
         r1_dictionary = r1.to_dictionary()
-        self.assertEqual(r1_dictionary, {'x': 1, 'y': 9, 'id': 12,
-                                         'height': 2, 'width': 10})
-                                         
+        self.assertEqual
+        (r1_dictionary, {'x': 1, 'y': 9, 'id': 12, 'height': 2, 'width': 10})
