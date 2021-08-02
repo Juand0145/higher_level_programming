d3c0769cf57bd12b651d74a55d588dd9f6ccc7dc
Juand0145 <juand0145@gmail.com>
2021-08-02 15:57:08 -0500

    Fixing semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 // Is a script that prints the addition of 2 integers
-function add (a, b){
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-const result = add (a, b);
+const result = add(a, b);
 
 console.log(result);
