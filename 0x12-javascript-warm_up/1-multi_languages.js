f626b8c0c75b874c44e9ad0ba8bc7721bca37dfd
Juand0145 <juand0145@gmail.com>
2021-08-02 10:31:39 -0500

    Adding task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Is a script that prints 3 lines
+const line1 = 'C is fun';
+const line2 = '\nPython is cool';
+const line3 = '\nJavaScript is amazing';
+
+console.log(line1, line2, line3);
