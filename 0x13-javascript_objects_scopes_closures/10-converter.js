a73fd66246c431bab19c3a089b90431f5c792de8
Juand0145 <juand0145@gmail.com>
2021-08-03 16:44:17 -0500

    Adding task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Is a function that converts a number from base 10 to another base passed as argument
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base);
+  };
+};
