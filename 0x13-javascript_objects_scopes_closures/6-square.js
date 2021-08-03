5acf00df8c46646ec0b6e18ddcbf43718023cb1c
Juand0145 <juand0145@gmail.com>
2021-08-03 10:58:16 -0500

    Adding task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/node
+
+// Is a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+const firstSquare = require('./5-square');
+
+class Square extends firstSquare {
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
