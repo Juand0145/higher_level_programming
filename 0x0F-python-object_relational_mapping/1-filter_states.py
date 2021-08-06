a9df8d96dc43a42f48da7ef1b4f6d581de76e6f1
Juand0145 <juand0145@gmail.com>
2021-08-06 15:31:13 -0500

    fixing pep8 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Is a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""Is a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
 if __name__ == '__main__':
 
     import MySQLdb
@@ -12,7 +13,8 @@ if __name__ == '__main__':
                          db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
