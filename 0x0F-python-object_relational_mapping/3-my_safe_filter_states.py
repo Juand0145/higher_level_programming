2bec9b9b20ea670621c51b3f18fff676daae0e15
Juand0145 <juand0145@gmail.com>
2021-08-06 16:38:30 -0500

    Addig task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Is a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE {argv[4]}\
+                ORDER BY states.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    db.close()
