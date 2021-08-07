e12c6bdb97abd889dc97b85a8ff346ae4ae372d9
Juand0145 <juand0145@gmail.com>
2021-08-07 10:31:36 -0500

    Fixing task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
                          user=argv[1],
                          passwd=argv[2],
-                         db=argv[3])
+                         db=argv[3],
+                         port=3306)
 
     cur = db.cursor()
     cur.execute(
