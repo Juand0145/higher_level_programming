a81afd7d96361493efe6faedfff1ae5a92074cfb
Juand0145 <juand0145@gmail.com>
2021-08-07 10:34:31 -0500

    Fixing task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,15 @@ from the database hbtn_0e_0_usa"""
 if __name__ == '__main__':
 
     import MySQLdb
-    from sys import argv
+    import sys
 
-    db = MySQLdb.connect(host='localhost',
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute(
-        f"SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+        """SELECT * FROM states\
+             WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    db.close()
