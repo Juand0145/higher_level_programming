c6fcd50a26ef5d2551bdd13a8f8703773ec197b5
Juand0145 <juand0145@gmail.com>
2021-08-20 17:44:57 -0500

    fixing task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
 
-    req = requests.get("https://api.github.com/user")
+    req = requests.get("https://intranet.hbtn.io/status")
 
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
