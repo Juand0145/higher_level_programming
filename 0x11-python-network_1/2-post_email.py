ca166bcf2cd34e7dfefad63c854602d268cb452c
Juand0145 <juand0145@gmail.com>
2021-08-20 14:53:33 -0500

    Fix adding task 0 and adding task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Is a Python script that takes in a URL and an email,
+ sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+import urllib.request
+from urllib.parse import urlparse
+from sys import argv
+
+if __name__ == "__main__":
+    email = urlparse({"email", argv[2]}).encode()
+    req = urllib.request(argv[1])
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf8"))
