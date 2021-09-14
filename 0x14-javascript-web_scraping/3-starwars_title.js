7357d0cacdc450fb4b59818edeb7b3268eb7e147
Juand0145 <juand0145@gmail.com>
2021-09-14 16:21:59 -0500

    Fixing task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const link = 'http://swapi.co/api/films/' + process.argv[2];
+const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request.get(link,
   function (err, response, body) {
-    if (err) console.log(err);
-    else console.log(JSON.parse(body).title);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).title);
+    }
   });
