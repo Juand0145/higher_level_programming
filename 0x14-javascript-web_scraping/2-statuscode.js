834fabade5f6b02bf5620b152bc3d0ebcc0421f5
Juand0145 <juand0145@gmail.com>
2021-09-14 17:34:27 -0500

    fixing semistandard 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,9 @@ const request = require('request');
 const link = process.argv[2];
 
 request(link, function (err, response) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log('code: ' + response.statusCode);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
