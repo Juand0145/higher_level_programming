ac83f6be2dfd98652696e9e447c2e3e711ff6db2
Juand0145 <juand0145@gmail.com>
2021-09-14 17:43:57 -0500

    adding task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(path, body, err => {
+      if (err) console.log(err);
+    });
+  }
+});
